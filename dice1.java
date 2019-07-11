60a59ec4a0275de5bd2b1138bb0661eaf711a0ae
s1260181 <s1260181@u-aizu.ac.jp>
2019-07-11 16:10:16 +0900

    Add one more new feature to your program

--- dice1.java
+++ dice1.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-	public class dice {
+	public class dice1 {
 		public static void main(String args[]) {
 			Random rad = new Random();
 			Scanner scan = new Scanner(System.in);
@@ -25,12 +25,57 @@ import java.util.Scanner;
 			System.out.println("Total value: " + total);
 
 			if(total > 7) {
-<<<<<<< HEAD
-=======
 				System.out.println(name + " win!");
 			}else {
 				System.out.println(name + " lost");
 			}
->>>>>>> experimental
-		}
+			System.out.println("Die 3: " + dice3);
+			System.out.println("Die 4: " + dice4);
+
+			if((dice1 > dice2) && (dice1 > dice3) && (dice1 > dice4)) {
+				System.out.println("dice1 is bigger");
+			}else if((dice2 > dice1) && (dice2 > dice3) && (dice2 > dice4)) {
+				System.out.println("dice2 is bigger");
+			}else if((dice3 > dice1) && (dice3 > dice2) && (dice3 > dice4)) {
+				System.out.println("dice3 is bigger");
+			}else if((dice4 > dice1) && (dice4 > dice2) && (dice4 > dice3)) {
+				System.out.println("dice4 is bigger");
+			}else if((dice3 > dice1) && (dice3 > dice2) && (dice3 > dice4)) {
+				System.out.println("dice3 is bigger");
+			}else if((dice3 == dice1) && (dice3 == dice2) && (dice3 == dice4)) {
+				System.out.println("All dice are biggest");
+			}else if((dice3 == dice1) && (dice3 == dice2)) {
+				System.out.println("dice1 and dice2 and dice3 are biggest");
+			}else if((dice2 == dice1) && (dice4 == dice2)) {
+				System.out.println("dice1 and dice2 and dice4 are biggest");
+			}else if((dice3 == dice1) && (dice3 == dice4)) {
+				System.out.println("dice1 and dice3 and dice4 are biggest");
+			}else if((dice3 == dice4) && (dice3 == dice2)) {
+				System.out.println("dice2 and dice3 and dice4 are biggest");
+			}else if((dice1 == dice2) && (dice3 == dice4) && (dice1 > dice3)) {
+				System.out.println("dice1 and dice2 are biggest");
+			}else if((dice1 == dice2) && (dice3 == dice4) && (dice1 < dice3)) {
+				System.out.println("dice3 and dice4 are biggest");
+			}else if((dice1 == dice3) && (dice2 == dice4) && (dice1 > dice2)) {
+				System.out.println("dice1 and dice3 are biggest");
+			}else if((dice1 == dice3) && (dice2 == dice4) && (dice1 < dice2)) {
+				System.out.println("dice2 and dice4 are biggest");
+			}else if((dice1 == dice4) && (dice2 == dice3) && (dice1 > dice2)) {
+				System.out.println("dice1 and dice4 are biggest");
+			}else if((dice1 == dice4) && (dice2 == dice3) && (dice1 < dice2)) {
+				System.out.println("dice2 and dice3 are biggest");
+			}else if(dice1 == dice2) {
+				System.out.println("dice1 and dice2 are biggest");
+			}else if(dice1 == dice3) {
+				System.out.println("dice1 and dice3 are biggest");
+			}else if(dice1 == dice4) {
+				System.out.println("dice1 and dice4 are biggest");
+			}else if(dice2 == dice3) {
+				System.out.println("dice2 and dice3 are biggest");
+			}else if(dice2 == dice4) {
+				System.out.println("dice2 and dice4 are biggest");
+			}else if(dice4 == dice3) {
+				System.out.println("dice3 and dice4 are biggest");
+			}
+       	}
 }		
