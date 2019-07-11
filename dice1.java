9fc57fd0a1affb4e8b51ce18408c203f0e03b257
s1260181 <s1260181@u-aizu.ac.jp>
2019-07-11 15:51:20 +0900

    Won/lost message modified

--- dice1.java
+++ dice1.java
@@ -1,16 +1,33 @@
 import java.util.Random;
-public class dice1{
+import java.util.Scanner;
+
+	public class dice {
 		public static void main(String args[]) {
 			Random rad = new Random();
+			Scanner scan = new Scanner(System.in);
 
 			int dice1 = rad.nextInt(6) + 1;
 			int dice2 = rad.nextInt(6) + 1;
 			int total = dice1 + dice2;
 
-						System.out.println("Rolling the dice...");
+			int dice3 = rad.nextInt(6) + 1;
+			int dice4 = rad.nextInt(6) + 1;
+
+			System.out.println("What is your name? ");
+			String name = scan.nextLine();
+			System.out.println("> Hello, " + name);
+			System.out.println("");
+
+			System.out.println("Rolling the dice...");
 			System.out.println("Die 1: " + dice1);
 			System.out.println("Die 2: " + dice2);
 
 			System.out.println("Total value: " + total);
+
+			if(total > 7) {
+				System.out.println(name + " win!");
+			}else {
+				System.out.println(name + " lost");
+			}
 		}
-}
+}		
